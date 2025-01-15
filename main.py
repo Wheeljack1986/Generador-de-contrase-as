15157bf0c09382924a903ee50184296a5cd805f0
Wheeljack1986 <bullonrod.alexadrian@gmail.com>
2025-01-14 19:57:30 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+contraseña = ""
+long = int(input("Introduzca la longitud de la contraseña: "))
+
+for i in range(long):
+    contraseña += random.choice(caracteres)
+
+print ("Tu contraseña es:", contraseña)
